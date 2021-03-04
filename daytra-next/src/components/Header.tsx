1901c568aa199d1a4ee5a6b6e5041d36088dfdcb
Yusuke Ito <maroemon58@gmail.com>
2021-03-05 08:55:05 +0900

    Update header component

--- daytra-next/src/components/Header.tsx
+++ daytra-next/src/components/Header.tsx
@@ -4,17 +4,55 @@ import styled from 'styled-components'
 export default function Header() {
   return (
     <>
-      <Link href="/">
-        <HeaderLink>HOME</HeaderLink>
-      </Link>
-      <Link href="/about">
-        <HeaderLink>ABOUT</HeaderLink>
-      </Link>
+      <HeaderContent>
+        <HeaderLogo>
+          <span>30DAYSトライアル</span>
+        </HeaderLogo>
+
+        <Menu>
+          <Link href="#">
+            <HeaderLink>デイトラとは</HeaderLink>
+          </Link>
+          <Link href="#">
+            <HeaderLink>コース一覧</HeaderLink>
+          </Link>
+          <Link href="#">
+            <HeaderLink>お問い合わせ</HeaderLink>
+          </Link>
+        </Menu>
+      </HeaderContent>
     </>
   )
 }
 
+const HeaderContent = styled.div`
+  margin: auto 100px;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const HeaderLogo = styled.div`
+  > span {
+    font-size: 24px;
+    font-weight: bold;
+  }
+`
+
+const Menu = styled.div`
+  display: flex;
+
+  > a {
+    &:not(:first-child) {
+      margin-left: 20px
+    }
+  }
+
+`
 const HeaderLink = styled.a`
   color: #000000;
   font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
 `
