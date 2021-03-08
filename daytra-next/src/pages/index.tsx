a462c7d806b2087b697d829f1f780ce1e9ff0c38
Yusuke Ito <maroemon58@gmail.com>
2021-03-08 09:28:35 +0900

    Add Courses component

--- daytra-next/src/pages/index.tsx
+++ daytra-next/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header';
 import MainVisual from '../modules/MainVisual';
 import About from '../modules/About';
+import Courses from '../modules/Courses';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header/>
       <MainVisual/>
       <About/>
+      <Courses/>
     </>
   )
 }
